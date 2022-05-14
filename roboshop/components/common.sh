eb8f9b3d9c9f89fa405320c14d6b917fe8b90b6c
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-05-14 17:54:52 +0530

    updating

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -282,10 +282,15 @@ SYSTEMD_SETUP() {
   echo "Fix App Permissions"
 chown roboshop:roboshop -R /home/roboshop
   sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/'\
-   / -e 's/REDIS_ENDPOINT/redis.roboshop.internal/'\
-    / -e  's/MONGO_ENDPOINT/mongodb.roboshop.internal/' \
-    / -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/'\
-     -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/'  /home/roboshop/${COMPONENT}/systemd.service
+   -e 's/REDIS_ENDPOINT/redis.roboshop.internal/'\
+    -e  's/MONGO_ENDPOINT/mongodb.roboshop.internal/' \
+     -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/'\
+     -e 's/CARTENDPOINT/cart.roboshop.internal/' \
+    -e 's/DBHOST/mysql.roboshop.internal/'\
+     -e 's/CARTHOST/cart.roboshop.internal/' \
+    -e 's/USERHOST/user.roboshop.internal/' \
+    -e 's/AMQPHOST/rabbitmq.roboshop.internal/' \
+     /home/roboshop/${COMPONENT}/systemd.service &>>$LOG_FILE
     STAT $?
 
     echo "Setup SystemD Service\t"
